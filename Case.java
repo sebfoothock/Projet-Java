6464dfc7ff81235b7af299a3fd7b280af5f0b5e8
massonjulien <32932842+massonjulien@users.noreply.github.com>
2017-11-14 11:49:33 +0100

    Case

---
+++ Case.java
@@ -0,0 +1,14 @@
+package roulette;
+
+public class Case {
+	int num;
+	String parity;
+	String color;
+	int intervals;
+	public Case(int num, String parity, String color, int intervals){
+		this.num = num;
+		this.parity = parity;
+		this.color = color;
+		this.intervals = intervals;
+	}
+}
