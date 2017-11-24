f88e736fd585bf695ce91ab837a062f514955bde
sebfoothock <32933260+sebfoothock@users.noreply.github.com>
2017-11-24 19:09:21 +0100

    Update ControllerParis.java

--- ProjetRoulette/src/controller/ControllerParis.java
+++ ProjetRoulette/src/controller/ControllerParis.java
@@ -1,4 +1,5 @@
 package controller;
+
 import java.awt.Dimension;
 import java.awt.EventQueue;
 
@@ -8,24 +9,17 @@ import view.ViewParisFenetre;
 public class ControllerParis {
 	private static ModelParis model;
 	private static ViewParisFenetre vue;
-	
-	public ControllerParis(ModelParis p){
+
+	public ControllerParis(ModelParis p) {
 		model = p;
 	}
-	
+
 	public static void main(String[] args) {
-		vue = new ViewParisFenetre();
-		EventQueue.invokeLater(new Runnable(){
-			public void run(){
-				try{
-					ViewParisFenetre frame = new ViewParisFenetre();
-					frame.setVisible(true);
-					frame.setTitle("vue");
-					frame.setSize(new Dimension(1050, 816));
-				} catch(Exception e){
-					e.printStackTrace();
-				}
-			}
-		});
+		ViewParisFenetre frame = new ViewParisFenetre();
+		frame.setVisible(true);
+		frame.setTitle("vue");
+		frame.setSize(new Dimension(1030, 803));
+		frame.setResizable(false);
+
 	}
 }
